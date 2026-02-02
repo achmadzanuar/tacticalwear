f42dccea75e5f50f1d2c974e385bbbd8a53d8d9d
Achmad Zanuar <alies.zanuar@gmail.com>
2026-02-02 14:13:12 +0700

    feat: full contentful rich text production setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Why from './components/Why';
 import ProductList from './components/ProductList';
+import SocialProof from './components/SocialProof';
 import CTA from './components/CTA';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Hero />
       <Why />
       <ProductList />
+      <SocialProof />
       <CTA />
     </>
   );
